f383a5cb79c185773490d0349354cba3d4db00f9
yyang <gdyangyu@gmail.com>
2020-10-23 11:53:47 +0800

    update

--- src/main/java/yang/yu/tmall/domain/OrderLine.java
+++ src/main/java/yang/yu/tmall/domain/OrderLine.java
@@ -1,10 +1,13 @@
 package yang.yu.tmall.domain;
 
 import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 
 @Embeddable
 public class OrderLine {
+
+    @ManyToOne
     private Product product;
     private BigDecimal quantity;
     private BigDecimal unitPrice;
