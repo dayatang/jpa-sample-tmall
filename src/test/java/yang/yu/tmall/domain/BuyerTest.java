4a161d15b6873416cc5f5bf7c15453bcc543102d
yyang <gdyangyu@gmail.com>
2020-11-12 20:31:47 +0800

    update

--- src/test/java/yang/yu/tmall/domain/BuyerTest.java
+++ src/test/java/yang/yu/tmall/domain/BuyerTest.java
@@ -1,20 +1,20 @@
 package yang.yu.tmall.domain;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class BuyerRepositoryTest {
+class BuyerRepositoryTest implements WithAssertions {
 
     private static EntityManagerFactory emf;
 
     private EntityManager entityManager;
+
+    private EntityTransaction transaction;
 
     private Buyer buyer1;
 
@@ -28,13 +28,29 @@ class BuyerRepositoryTest {
     @BeforeEach
     void beforeEach() {
         entityManager = emf.createEntityManager();
+        transaction = entityManager.getTransaction();
+        transaction.begin();
+    }
+
+    @AfterEach
+    void tearDown() {
+        transaction.rollback();
+        entityManager.clear();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        emf.close();
     }
 
     @Test
     void create() {
         buyer1 = new Buyer("buyer1");
         buyer2 = new Buyer("buyer2");
-
+        entityManager.persist(buyer1);
+        entityManager.persist(buyer2);
+        assertThat(buyer1.getId()).isGreaterThan(0);
+        assertThat(buyer2.getId()).isGreaterThan(0);
     }
 
 }
